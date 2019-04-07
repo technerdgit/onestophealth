3fd42a4a021feecd3c7bf3ebe46f29bd47b58a0f
KB Total Recall <kanwar@gmail.com>
2019-04-07 10:04:27 -0700

    Add logic for New Patient list on Doctor

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,36 @@
-var Sequelize  = require ("sequelize");
+var Sequelize = require("sequelize");
 var db = require("../models");
 
 module.exports = function (app) {
     app.get("/api/doctors/:id", function (req, res) {
-      
-      db.doctors.findAll({
-        where: {
-            id: req.params.id
-        },
-        include: [{
-            model: db.patients
-        }]
-    }).then(function (dbDoctor) {
-        var docdata={};
-        var dataObj=[];
-        for(i=0;i < dbDoctor[0].patients.length;i++) {
-        var patient_name = dbDoctor[0].patients[i].dataValues.patient_name;
-        var current_patient = dbDoctor[0].patients[i].patient_doctors.dataValues.current_patient;
-         docdata = {
-            current_patient: current_patient,
-            patient_name: patient_name
-         };
-         dataObj.push(docdata);
-       };           
-        var doctorObj = {
-            doctors: dataObj
-        }
-    
-        res.render('doctors', doctorObj);
+
+        db.doctors.findAll({
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: db.patients
+            }]
+        }).then(function (dbDoctor) {
+            var docdata = {};
+            var dataObj = [];
+            for (i = 0; i < dbDoctor[0].patients.length; i++) {
+                var patient_name = dbDoctor[0].patients[i].dataValues.patient_name;
+                var current_patient = dbDoctor[0].patients[i].patient_doctors.dataValues.current_patient;
+                var patient_request = dbDoctor[0].patients[i].patient_doctors.dataValues.patient_request;
+                docdata = {
+                    current_patient: current_patient,
+                    patient_name: patient_name,
+                    patient_request: patient_request
+                };
+                console.log(patient_request);
+                dataObj.push(docdata);
+            };
+            var doctorObj = {
+                doctors: dataObj
+            }
+           
+            res.render('doctors', doctorObj);
         });
 
     });
