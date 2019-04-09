b803e5c1d4455f9aa1b04ecaa24e700065f76da5
KB Total Recall <kanwar@gmail.com>
2019-04-09 13:21:36 -0700

    Push change for the doctor redirect to dashboard

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
 			data: newDoctor
 	}).then(
 	function(response){
-		parent.window.location="/api/doctor/"+ response.id;
+		parent.window.location="/api/doctors/"+ response.id;
 	});
   });
 });
